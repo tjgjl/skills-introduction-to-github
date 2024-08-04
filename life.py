239a9b0cb3f4447f1d2a7ae38c6d87617d90ce61
tjgjl <161323085+tjgjl@users.noreply.github.com>
2024-08-04 18:08:32 +0800

    Update life.py

--- life.py
+++ life.py
@@ -12,8 +12,7 @@ setup(
 
   
 
-    url='https://github.com/Tongjilibo/bert4torch',
-    author='Tongjilibo',
+   
     install_requires=['numpy', 'tqdm', 'torch>1.6', 'torch4keras==0.2.4', 'six'],
     packages=find_packages()
 )
